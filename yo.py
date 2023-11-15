caf7377d7e792e067cdaac0d97e7a81ea7786080
renalds231 <renalds2006@inbox.lv>
2023-11-15 10:46:58 +0200

    Pievienot summas aprekinu

---
+++ yo.py
@@ -0,0 +1,10 @@
+'''Izveidt uzd kur ierakstot skaitli izvada to summu           '''
+
+#lietotajs ievada divus skaitlus
+skaitlis1 = float(input("Ievadiet pirmo skaitli: "))
+skaitlis2 = float(input("Ievadiet otro skaitli: "))
+
+# aprekina un izvada
+
+summa = skaitlis1 + skaitlis2
+print(f"Summa: {summa}")
